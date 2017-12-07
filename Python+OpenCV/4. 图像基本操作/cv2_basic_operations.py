eed0d7a453ec88afdf01dd610eb724ac57c122ac
ex2tron <ex2tron@foxmail.com>
2017-12-07 14:46:45 +0800

    Python+OpenCV教程4 2017年12月7日

---
+++ Python+OpenCV/4. 图像基本操作/cv2_basic_operations.py
@@ -0,0 +1,37 @@
+import cv2
+
+img = cv2.imread('lena.jpg')
+
+# 获取像素的值
+px = img[100, 100]
+print(px)  # [119 108 201]
+
+# 只获取蓝色blue通道的值
+px_blue = img[100, 100, 0]
+print(px_blue)  # 119
+
+# 修改像素的值
+img[100, 100] = [255, 255, 255]
+print(img[100, 100])  # [255 255 255]
+
+# 图片形状
+print(img.shape)  # (263, 263, 3)
+# 形状中包括高度、宽度和通道数
+height, width, channels = img.shape
+# 总像素数
+print(img.size)  # 263*263*3=207507
+# 数据类型
+print(img.dtype)  # uint8
+
+# ROI截取
+face = img[100:200, 115:188]
+cv2.imshow('face', face)
+cv2.waitKey(0)
+
+# 通道分割与合并
+b, g, r = cv2.split(img)
+img = cv2.merge((b, g, r))
+# 更推荐的获取某一通道方式
+b = img[:, :, 0]
+cv2.imshow('b', b)
+cv2.waitKey(0)
