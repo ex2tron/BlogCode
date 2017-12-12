3a999e474efe72095d59875f97a8a5ab534b3221
ex2tron <ex2tron@foxmail.com>
2017-12-12 13:05:11 +0800

    Python+OpenCV教程1修改

--- Python+OpenCV/12. 亮度和对比度/cv2_contrast_brightness.py
+++ Python+OpenCV/12. 亮度和对比度/cv2_contrast_brightness.py
@@ -7,7 +7,7 @@ img = cv2.imread('lena.jpg')
 # numpy默认是取模运算，250+10 = 260%256 = 4
 # 所以此处需要用np.clip进行范围限定，
 # a<0 => a=0, a>255 => a=255
-res = np.uint8(np.clip((1.5 * img + 0), 0, 255))
+res = np.uint8(np.clip((1.5 * img + 10), 0, 255))
 
 # 两张图片横向合并（便于对比显示）
 tmp = np.hstack((img, res))
