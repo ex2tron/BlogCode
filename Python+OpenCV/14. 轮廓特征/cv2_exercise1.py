950c7839695dc6a339c5433344e7f8b8e28470f5
ex2tron <ex2tron@foxmail.com>
2017-12-22 14:18:03 +0800

    Python+OpenCV教程调整

--- Python+OpenCV/14. 轮廓特征/cv2_exercise1.py
+++ Python+OpenCV/14. 轮廓特征/cv2_exercise1.py
@@ -12,24 +12,24 @@ image, contours, hierarchy = cv2.findContours(thresh, 3, 2)
 img_color1 = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 img_color2 = np.copy(img_color1)
 
-# 以数字3的轮廓为例
-cnt = contours[0]
+# 计算数字1的轮廓特征
+cnt = contours[1]
 cv2.drawContours(img_color1, [cnt], 0, (0, 0, 255), 2)
 
 # 1.轮廓面积
-area = cv2.contourArea(cnt)  # 4386.5
+area = cv2.contourArea(cnt)  # 6289.5
 print(area)
 
 
 # 2.轮廓周长
-perimeter = cv2.arcLength(cnt, True)  # 585.7716
+perimeter = cv2.arcLength(cnt, True)  # 527.4041
 print(perimeter)
 
 
 # 3.图像矩
 M = cv2.moments(cnt)
 print(M)
-print(M['m00'])  # 同前面的面积：4386.5
+print(M['m00'])  # 同前面的面积：6289.5
 cx, cy = M['m10'] / M['m00'], M['m01'] / M['m00']  # 质心
 print(cx, cy)
 
@@ -39,7 +39,6 @@ cv2.rectangle(img_color1, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 rect = cv2.minAreaRect(cnt)  # 最小外接矩形
 box = np.int0(cv2.boxPoints(rect))  # 矩形的四个角点并取整
-# 也可以用astype(np.int)取整
 cv2.drawContours(img_color1, [box], 0, (255, 0, 0), 2)
 
 cv2.imshow('contours', img_color1)
@@ -48,8 +47,7 @@ cv2.waitKey(0)
 
 # 5.最小外接圆
 (x, y), radius = cv2.minEnclosingCircle(cnt)
-(x, y, radius) = np.int0((x, y, radius))
-# 或使用这句话取整：(x, y, radius) = map(int, (x, y, radius))
+(x, y, radius) = map(int, (x, y, radius))  # 这也是取整的一种方式噢
 cv2.circle(img_color2, (x, y), radius, (0, 0, 255), 2)
 
 
@@ -59,15 +57,3 @@ cv2.ellipse(img_color2, ellipse, (0, 255, 0), 2)
 
 cv2.imshow('contours2', img_color2)
 cv2.waitKey(0)
-
-
-# 7.形状匹配
-img = cv2.imread('shapes.jpg', 0)
-_, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-image, contours, hierarchy = cv2.findContours(thresh, 3, 2)
-img_color = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-
-cnt_a, cnt_b, cnt_c = contours[0], contours[1], contours[2]
-print(cv2.matchShapes(cnt_b, cnt_b, 1, 0.0))  # 0.0
-print(cv2.matchShapes(cnt_b, cnt_c, 1, 0.0))  # 2.17e-05
-print(cv2.matchShapes(cnt_b, cnt_a, 1, 0.0))  # 0.418
