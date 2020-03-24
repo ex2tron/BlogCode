34cd3c8830cff2e525301ceef923c6bbb86b8924
extronwang <extronwang@tencent.com>
2020-03-24 18:10:24 +0800

    添加ignore文件

--- WeChat-H5-Questionnaire/demo2/miniprogram/index/index.js
+++ WeChat-H5-Questionnaire/demo2/miniprogram/index/index.js
@@ -10,7 +10,8 @@ Page({
     },
 
     navH5: function() {
-        const url = "http://127.0.0.1:5500/index.html";
+        // const url = "http://127.0.0.1:5500/index.html";
+        const url = "http://codec.wang/h5-questionnaire/";
 
         wx.navigateTo({
             url: `../h5/h5?url=${url}`,
